5e1df0bc726cd73b32af7a8d3de3d423899cf832
AllouYao <allouyao713@outlook.com>
2024-01-05 17:08:14 +0000

    "Blocage des routes"

--- src/EventListener/CampagneEventListener.php
+++ src/EventListener/CampagneEventListener.php
@@ -5,44 +5,79 @@ namespace App\EventListener;
 use App\Repository\Paiement\CampagneRepository;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Flasher\Prime\FlasherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Routing\RouterInterface;
 
 class CampagneEventListener
 {
     private $flasher;
     private $router;
+    private $campagneRepository;
 
 
 
-    public function __construct(FlasherInterface $flasher,RouterInterface $router)
+    public function __construct(FlasherInterface $flasher,RouterInterface $router, CampagneRepository $campagneRepository)
     {
         $this->flasher = $flasher;
         $this->router = $router;
+        $this->campagneRepository = $campagneRepository;
+
 
     }
 
-    public function onKernelRequest(RequestEvent $event,CampagneRepository $campagneRepository)
+    public function onKernelRequest(RequestEvent $event)
     {
-        dd($event);
-        $routes = ['personal_heure_sup_new'];
-        $currentRoute = $event->getRequest()->attributes->get('_route');
-        $campagneActives = $campagneRepository->getCampagneActives();
+        
 
 
-        if (!$campagneActives) {
+        $currentRoute = $event->getRequest()->getPathInfo();
 
-            foreach ($routes as $route) {
+
+        $campagneActives = $this->campagneRepository->getCampagneActives();
+
+
+        if ($campagneActives) {
+
+                // checking path endWith new and edit with current path
+            if ($this->checkRoute($currentRoute)) {
+
+                $this->flasher->addFlash('error', "vous avez une campagne en cours: Aucune modifictaion possible !");
+
+                $url = $this->router->generate('app_home');
                 
-                if ($currentRoute == $route) {
+                //flash()->addFlash('error', "vous avez une campagne en cours: Aucune modifictaion possible !");
+                $response = new RedirectResponse($url);
 
-                    //$this->flasher->addFlash('error', "Aucune modifcation n'est possible ");
-                   // return $this->redirectToRoute('app_home');
-                }
+                $event->setResponse($response);
             }
             
         }
 
+
         
     }
+
+    private function checkRoute($currentRoute):bool
+    {
+        
+        $allRoutes = $this->router->getRouteCollection()->all();
+
+        $routes = array_filter($allRoutes, function ($route) {
+            $path = $route->getPath();
+            return substr($path, -4) === 'edit' || substr($path, -3) === 'new';
+        });
+
+        foreach($routes as $route) {
+
+            if ($route->getPath() == $currentRoute) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
 }
