f74fbe5645d859f5b9acc66fd65c4bf03fe7e8d8
Offset Dev <offset-dev@offset-consulting.com>
2024-03-04 17:19:35 +0000

    Fix error schedule task

--- src/Scheduler/UpdateGratification.php
+++ src/Scheduler/UpdateGratification.php
@@ -2,22 +2,56 @@
 
 namespace App\Scheduler;
 
+use App\Repository\DossierPersonal\PersonalRepository;
+use App\Repository\Settings\PrimesRepository;
+use App\Service\Personal\ChargesServices;
+use App\Utils\Status;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Scheduler\Attribute\AsCronTask;
+use Zenstruck\ScheduleBundle\Attribute\AsScheduledTask;
+
+#[AsScheduledTask('* * * * *')]
 final class UpdateGratification
 {
-    /*
-     * Add whatever properties and methods you need
-     * to hold the data for this message class.
-     */
+    private PersonalRepository $personalRepository;
+    private PrimesRepository $primesRepository;
+    private EntityManagerInterface $entityManager;
+    private ChargesServices $chargesServices;
+    private LoggerInterface $logger;
 
-//     private $name;
+    public function __construct(
+        PersonalRepository     $personalRepository,
+        PrimesRepository       $primesRepository,
+        EntityManagerInterface $entityManager,
+        ChargesServices        $chargesServices,
+        LoggerInterface        $logger
+    )
+    {
+        $this->personalRepository = $personalRepository;
+        $this->primesRepository = $primesRepository;
+        $this->entityManager = $entityManager;
+        $this->chargesServices = $chargesServices;
+        $this->logger = $logger;
+    }
 
-//     public function __construct(string $name)
-//     {
-//         $this->name = $name;
-//     }
-
-//    public function getName(): string
-//    {
-//        return $this->name;
-//    }
+    public function __invoke(): void
+    {
+        $personal = $this->personalRepository->findAll();
+        $tauxGratification = (int)$this->primesRepository->findOneBy(['code' => Status::GRATIFICATION])->getTaux();
+        foreach ($personal as $item) {
+            $olderMonth = $item->getOlder() * 12;
+            $service = $this->chargesServices->amountSalaireBrutAndImposable($item);
+            $salaireCategoriel = $service['salaire_categoriel'];
+            if ($olderMonth < 12) {
+                $gratification = ((($salaireCategoriel * $tauxGratification) / 100) * ($olderMonth * 30)) / 360;
+            } else {
+                $gratification = ($salaireCategoriel * $tauxGratification) / 100;
+            }
+            $item->getSalary()->setGratification($gratification);
+            $this->entityManager->persist($item);
+        }
+        $this->entityManager->flush();
+        $this->logger->info(sprintf("Tâche executée avec succès %s", (new \DateTime())->format('d/M/Y')));
+    }
 }
