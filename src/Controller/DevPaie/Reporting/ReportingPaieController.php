23ebdb93bb46d3ceaac023808a7ed9acb280c0ba
Offset Dev <offset-dev@offset-consulting.com>
2024-03-11 15:22:39 +0000

    Mise à jour, implementation de l'état des retenues sur salaire et validation

--- src/Controller/DevPaie/Reporting/ReportingPaieController.php
+++ src/Controller/DevPaie/Reporting/ReportingPaieController.php
@@ -14,4 +14,22 @@ class ReportingPaieController extends AbstractController
     {
         return $this->render('dev_paie/reportings/operations/remboursement/remboursement.html.twig');
     }
+
+    #[Route('/validated_remboursement_salaire', name: 'validated_remboursement', methods: ['GET', 'POST'])]
+    public function viewValidateRemboursement(): Response
+    {
+        return $this->render('dev_paie/reportings/operations/remboursement/remboursement.validated.html.twig');
+    }
+
+    #[Route('/retenue_salaires', name: 'retenue_salaires', methods: ['GET', 'POST'])]
+    public function viewRetenueSalaire(): Response
+    {
+        return $this->render('dev_paie/reportings/operations/retenue_salaire/retenue_salaries.html.twig');
+    }
+
+    #[Route('/validated_retenue_salaires', 'validated_retenue_salaires', methods: ['GET', 'POST'])]
+    public function viewValidateRetenueSalaire(): Response
+    {
+        return $this->render('dev_paie/reportings/operations/retenue_salaire/retenue.validated.html.twig');
+    }
 }
